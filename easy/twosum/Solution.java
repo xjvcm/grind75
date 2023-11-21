efdeb791109b455f5a259b517becedddd5d7b8e9
Jonathan Manzano <jonvincruman@gmail.com>
2023-11-20 19:17:16 -0800

    Initial commit

---
+++ easy/twosum/Solution.java
@@ -0,0 +1,11 @@
+/**
+ * @author Jonathan Manzano
+ * @version Java 21 OpenJDK
+ */
+package easy.twosum;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
